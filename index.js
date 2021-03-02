266cdc3f7b727b2ea6c21a09288776d8bde4e578
Nahum <losond8@gmail.com>
2021-03-02 14:59:32 -0500

    commit tree 877d157a87f0a17301104ea675ea2f9246b18644

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = "Programmer"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+const firstInitial = currentUser[0];
+let restOfName = currentUser.slice(1,0);
+
+const shortGreeting = `Welcome, ${firstInitial}!`
+
